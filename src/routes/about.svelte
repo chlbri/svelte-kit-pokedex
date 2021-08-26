6f861c94ddd43ebf86ac730465bae7f850d6edba
chlbri <bri_lvi@icloud.com>
2021-08-26 23:22:57 +0000

    ok

--- src/routes/about.svelte
+++ src/routes/about.svelte
@@ -1,2 +1,5 @@
 <h1 class="text-4xl text-center my-8">About</h1>
-<p>This is a cool demo app that uses Svelte Kit to build a Pokedex web application.</p>
+<p>
+  This is a cool demo app that uses Svelte Kit to build a Pokedex web
+  application.
+</p>
